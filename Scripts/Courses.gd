e03976ee245cd569af808353ebfe5ef663431f77
dittygoops <aditya.gup16@gmail.com>
2024-02-14 01:49:08 -0700

    Lots of fixing, add menus, functional buttons.

---
+++ Scripts/Courses.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_calculus_i_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Calculus_I.tscn");
+
+func _on_calculus_ii_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Calculus_II.tscn");
+
+func _on_calculus_iii_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Calculus_III.tscn");
+
+func _on_differential_equations_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Differential_Equations.tscn");
